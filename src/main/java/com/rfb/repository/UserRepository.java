cbdf91cf638b247f86a7dc8013602e5cd1ee0030
Yu-Chi Lai <yuchilai88@gmail.com>
2021-03-29 16:28:41 -0400

    Initial version of rfbloyalty generated by JHipster-7.0.0

---
+++ src/main/java/com/rfb/repository/UserRepository.java
@@ -0,0 +1,147 @@
+package com.rfb.repository;
+
+import static org.springframework.data.relational.core.query.Criteria.where;
+import static org.springframework.data.relational.core.query.Query.query;
+
+import com.rfb.domain.Authority;
+import com.rfb.domain.User;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.convert.R2dbcConverter;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.relational.core.sql.Column;
+import org.springframework.data.relational.core.sql.Expression;
+import org.springframework.data.relational.core.sql.Table;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
+
+/**
+ * Spring Data R2DBC repository for the {@link User} entity.
+ */
+@Repository
+public interface UserRepository extends R2dbcRepository<User, Long>, UserRepositoryInternal {
+    Mono<User> findOneByActivationKey(String activationKey);
+
+    Flux<User> findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(LocalDateTime dateTime);
+
+    Mono<User> findOneByResetKey(String resetKey);
+
+    Mono<User> findOneByEmailIgnoreCase(String email);
+
+    Mono<User> findOneByLogin(String login);
+
+    Flux<User> findAllByIdNotNull(Pageable pageable);
+
+    Flux<User> findAllByIdNotNullAndActivatedIsTrue(Pageable pageable);
+
+    Mono<Long> count();
+
+    @Query("INSERT INTO jhi_user_authority VALUES(:userId, :authority)")
+    Mono<Void> saveUserAuthority(Long userId, String authority);
+
+    @Query("DELETE FROM jhi_user_authority")
+    Mono<Void> deleteAllUserAuthorities();
+}
+
+interface DeleteExtended<T> {
+    Mono<Void> delete(T user);
+}
+
+interface UserRepositoryInternal extends DeleteExtended<User> {
+    Mono<User> findOneWithAuthoritiesByLogin(String login);
+
+    Mono<User> findOneWithAuthoritiesByEmailIgnoreCase(String email);
+}
+
+class UserRepositoryInternalImpl implements UserRepositoryInternal {
+
+    private final DatabaseClient db;
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+    private final R2dbcConverter r2dbcConverter;
+
+    public UserRepositoryInternalImpl(DatabaseClient db, R2dbcEntityTemplate r2dbcEntityTemplate, R2dbcConverter r2dbcConverter) {
+        this.db = db;
+        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
+        this.r2dbcConverter = r2dbcConverter;
+    }
+
+    @Override
+    public Mono<User> findOneWithAuthoritiesByLogin(String login) {
+        return findOneWithAuthoritiesBy("login", login);
+    }
+
+    @Override
+    public Mono<User> findOneWithAuthoritiesByEmailIgnoreCase(String email) {
+        return findOneWithAuthoritiesBy("email", email.toLowerCase());
+    }
+
+    private Mono<User> findOneWithAuthoritiesBy(String fieldName, Object fieldValue) {
+        return db
+            .sql("SELECT * FROM jhi_user u LEFT JOIN jhi_user_authority ua ON u.id=ua.user_id WHERE u." + fieldName + " = :" + fieldName)
+            .bind(fieldName, fieldValue)
+            .map(
+                (row, metadata) ->
+                    Tuples.of(r2dbcConverter.read(User.class, row, metadata), Optional.ofNullable(row.get("authority_name", String.class)))
+            )
+            .all()
+            .collectList()
+            .filter(l -> !l.isEmpty())
+            .map(
+                l -> {
+                    User user = l.get(0).getT1();
+                    user.setAuthorities(
+                        l
+                            .stream()
+                            .filter(t -> t.getT2().isPresent())
+                            .map(
+                                t -> {
+                                    Authority authority = new Authority();
+                                    authority.setName(t.getT2().get());
+                                    return authority;
+                                }
+                            )
+                            .collect(Collectors.toSet())
+                    );
+                    return user;
+                }
+            );
+    }
+
+    @Override
+    public Mono<Void> delete(User user) {
+        return db
+            .sql("DELETE FROM jhi_user_authority WHERE user_id = :userId")
+            .bind("userId", user.getId())
+            .then()
+            .then(r2dbcEntityTemplate.delete(User.class).matching(query(where("id").is(user.getId()))).all().then());
+    }
+}
+
+class UserSqlHelper {
+
+    static List<Expression> getColumns(Table table, String columnPrefix) {
+        List<Expression> columns = new ArrayList<>();
+        columns.add(Column.aliased("id", table, columnPrefix + "_id"));
+        columns.add(Column.aliased("login", table, columnPrefix + "_login"));
+        columns.add(Column.aliased("password_hash", table, columnPrefix + "_password"));
+        columns.add(Column.aliased("first_name", table, columnPrefix + "_first_name"));
+        columns.add(Column.aliased("last_name", table, columnPrefix + "_last_name"));
+        columns.add(Column.aliased("email", table, columnPrefix + "_email"));
+        columns.add(Column.aliased("activated", table, columnPrefix + "_activated"));
+        columns.add(Column.aliased("lang_key", table, columnPrefix + "_lang_key"));
+        columns.add(Column.aliased("image_url", table, columnPrefix + "_image_url"));
+        columns.add(Column.aliased("activation_key", table, columnPrefix + "_activation_key"));
+        columns.add(Column.aliased("reset_key", table, columnPrefix + "_reset_key"));
+        columns.add(Column.aliased("reset_date", table, columnPrefix + "_reset_date"));
+        return columns;
+    }
+}
